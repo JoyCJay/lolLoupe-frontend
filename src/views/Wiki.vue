f7b2b40bfb23158a8d0efff040541fed1242b4ce
joycjay-R11 <joycjay@163.com>
2020-11-03 17:57:52 +0100

    FEAT: router assignment

--- src/views/Wiki.vue
+++ src/views/Wiki.vue
@@ -1,6 +1,60 @@
 <template>
-    <div class="about">
-        <h1>This is an wiki page</h1>
-    </div>
-    <router-view></router-view>
+    <a-layout-sider width="200" style="background: #fff">
+        <a-menu mode="inline" v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" style="height: 100%">
+            <a-sub-menu key="sub1">
+                <template v-slot:title>
+                    <span><user-outlined />subnav 1</span>
+                </template>
+                <a-menu-item key="1">option1</a-menu-item>
+                <a-menu-item key="2">option2</a-menu-item>
+                <a-menu-item key="3">option3</a-menu-item>
+                <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+                <template v-slot:title>
+                    <span><laptop-outlined />subnav 2</span>
+                </template>
+                <a-menu-item key="5">option5</a-menu-item>
+                <a-menu-item key="6">option6</a-menu-item>
+                <a-menu-item key="7">option7</a-menu-item>
+                <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+                <template v-slot:title>
+                    <span><notification-outlined />subnav 3</span>
+                </template>
+                <a-menu-item key="9">option9</a-menu-item>
+                <a-menu-item key="10">option10</a-menu-item>
+                <a-menu-item key="11">option11</a-menu-item>
+                <a-menu-item key="12">option12</a-menu-item>
+            </a-sub-menu>
+        </a-menu>
+    </a-layout-sider>
+    <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+        <div class="about">
+            <h1>This is an wiki page</h1>
+        </div>
+        <router-view />
+    </a-layout-content>
 </template>
+
+<script lang="ts">
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from "@ant-design/icons-vue";
+
+export default {
+    data() {
+        return {
+            selectedKeys1: ["2"],
+            selectedKeys2: ["1"],
+            openKeys: ["sub1"]
+        };
+    },
+    components: {
+        UserOutlined,
+        LaptopOutlined,
+        NotificationOutlined
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
